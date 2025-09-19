09c76aa0842e90e805a505ab72f222ef20d69a1c
plinadev <pavlenkopolina415@gmail.com>
2025-09-19 17:45:25 +0300

    fix apiClient configuration

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/api",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
